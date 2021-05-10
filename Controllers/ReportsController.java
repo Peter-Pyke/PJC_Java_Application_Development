85e9b46b5d4fa9abdeadbd61a4a285afeefb920f
Peter <72401515+Peter-Pyke@users.noreply.github.com>
2021-05-10 12:55:22 -0400

    Finished Reports
    All three reports are set up. I have reports by type and month. Reports by location and reports by contacts. The buttons for contacts and my choice reports were changed to combo boxes.

--- Controllers/ReportsController.java
+++ Controllers/ReportsController.java
@@ -1,19 +1,38 @@
 package Controllers;
 
 import DBAccess.DBAppointments;
+import DBAccess.DBContacts;
 import Model.Appointments;
+import Model.Contacts;
 import Model.Reports;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Timestamp;
 import java.time.Month;
+import java.util.ResourceBundle;
 
-public class ReportsController {
+public class ReportsController implements Initializable {
+    @FXML
+    private ComboBox<Contacts> contactsComboBox;
+    @FXML
+    private ComboBox<String> locationComboBox;
+
     @FXML
     private TableView<Reports> reportTableView;
 
@@ -27,41 +46,91 @@ public class ReportsController {
     private TableColumn<Reports, Integer> C3;
 
     @FXML
-    private TableColumn<Appointments, ?> C4;
+    private TableColumn<Reports, String> C4;
 
     @FXML
-    private TableColumn<Appointments, ?> C5;
+    private TableColumn<Reports, String> C5;
 
     @FXML
-    private TableColumn<Appointments, ?> C6;
+    private TableColumn<Reports, String> C6;
 
     @FXML
-    private TableColumn<Appointments, ?> C7;
+    private TableColumn<Reports, String> C7;
 
     @FXML
-    private TableColumn<Appointments, ?> C8;
+    private TableColumn<Reports, Integer> C8;
 
     @FXML
-    private TableColumn<Appointments, ?> C9;
+    private TableColumn<Reports, Integer> C9;
 
+    public void forMatTableColumns(){
+        C1.setText("AppointmentID");
+        C1.setCellValueFactory(new PropertyValueFactory<>("AppointmentID"));
+        C2.setText("Title");
+        C2.setCellValueFactory(new PropertyValueFactory<>("Title"));
+        C3.setText("Description");
+        C3.setCellValueFactory(new PropertyValueFactory<>("Description"));
+        C4.setText("Location");
+        C4.setCellValueFactory(new PropertyValueFactory<>("Location"));
+        C5.setText("Type");
+        C5.setCellValueFactory(new PropertyValueFactory<>("Type"));
+        C6.setText("Start");
+        C6.setCellValueFactory(new PropertyValueFactory<>("Start"));
+        C7.setText("End");
+        C7.setCellValueFactory(new PropertyValueFactory<>("End"));
+        C8.setText("ContactID");
+        C8.setCellValueFactory(new PropertyValueFactory<>("ContactID"));
+        C9.setText("CustomerID");
+        C9.setCellValueFactory(new PropertyValueFactory<>("CustomerID"));
+    }
 
+    public ObservableList<Reports> convertAppToReport() {
+        ObservableList<Appointments> allAppointments = DBAppointments.getAllAppointments();
+        ObservableList<Reports> reportsList = FXCollections.observableArrayList();
+        for (int i = 0; i < allAppointments.size(); i++) {
+            Appointments appointment = allAppointments.get(i);
+            int appID = appointment.getAppointmentID();
+            String appTitle = appointment.getTitle();
+            String appDes = appointment.getDescription();
+            String appLocation = appointment.getLocation();
+            String appType = appointment.getType();
+            Timestamp appStart = appointment.getStart();
+            Timestamp appEnd = appointment.getEnd();
+            int appContactID = appointment.getContactID();
+            int appUserID = appointment.getUserID();
+            Reports report = new Reports(appID, appTitle, appDes, appLocation, appType, appStart, appEnd, appContactID, appUserID);
+            reportsList.add(report);
+        }
+        return reportsList;
+    }
     @FXML
-    void onActionContactSchedule(ActionEvent event) {
+    void onActionContactsComboBox(ActionEvent event) {
 
+        ObservableList<Reports> allReports = convertAppToReport();
+        FilteredList<Reports> reportsForTable = new FilteredList<>(allReports, i -> i.getContactID() == contactsComboBox.getSelectionModel().getSelectedItem().getContactID());
+        reportTableView.setItems(reportsForTable);
+        forMatTableColumns();
     }
 
     @FXML
-    void onActionMainMenu(ActionEvent event) {
-
+    void onActionMainMenu(ActionEvent event) throws IOException{
+        Stage stage;
+        Parent scene;
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/Scenes/Main_Scene.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    void onActionMyChoice(ActionEvent event) {
-
+    void onActionLocationComboBox(ActionEvent event) {
+        ObservableList<Reports> allReports = convertAppToReport();
+        FilteredList<Reports> reportsForTable = new FilteredList<>(allReports, i -> i.getLocation().equals(locationComboBox.getSelectionModel().getSelectedItem()));
+        reportTableView.setItems(reportsForTable);
+        forMatTableColumns();
     }
-
     @FXML
-    void onActionTypeMonth(ActionEvent event) {
+    void onActionTypeMonth(ActionEvent event) throws IOException {
             ObservableList<Appointments> allAppointments = DBAppointments.getAllAppointments();
             ObservableList<Reports> myReport = FXCollections.observableArrayList();
             ObservableList<String> uniqueAppTyps = FXCollections.observableArrayList();
@@ -72,12 +141,11 @@ public class ReportsController {
                     uniqueAppTyps.add(type);
                 }
             }
-        for(int inner = 0; inner < uniqueAppTyps.size(); inner++){
-            String appType = uniqueAppTyps.get(inner);
-            System.out.println(appType);
+        for(int i = 0; i < uniqueAppTyps.size(); i++){
+            String appType = uniqueAppTyps.get(i);
 
-            for(int i = 1; i < 13; i++){
-                Month month = Month.of(i);
+            for(int inner = 1; inner < 13; inner++){
+                Month month = Month.of(inner);
                 int count = 0;
 
                     for(int inner2 = 0; inner2 < allAppointments.size(); inner2++){
@@ -92,6 +160,7 @@ public class ReportsController {
                     }
                 }
             }
+
             reportTableView.setItems(myReport);
             C1.setCellValueFactory(new PropertyValueFactory<>("month"));
             C1.setText("Month");
@@ -99,8 +168,36 @@ public class ReportsController {
             C2.setText("Type");
             C3.setCellValueFactory(new PropertyValueFactory<>("count"));
             C3.setText("Number of Appointments");
-        }
+            C4.setCellValueFactory(new PropertyValueFactory<>(""));
+            C4.setText("C4");
+            C5.setCellValueFactory(new PropertyValueFactory<>(""));
+            C5.setText("C5");
+            C6.setCellValueFactory(new PropertyValueFactory<>(""));
+            C6.setText("C6");
+            C7.setCellValueFactory(new PropertyValueFactory<>(""));
+            C7.setText("C7");
+            C8.setCellValueFactory(new PropertyValueFactory<>(""));
+            C8.setText("C8");
+            C9.setCellValueFactory(new PropertyValueFactory<>(""));
+            C9.setText("C9");
 
+        }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        contactsComboBox.setItems(DBContacts.getAllContacts());
+
+        ObservableList<Appointments> allAppointments = DBAppointments.getAllAppointments();
+        ObservableList<String> locations = FXCollections.observableArrayList();
+        for (int i = 0; i < allAppointments.size(); i++) {
+            String location = allAppointments.get(i).getLocation();
+            if(!(locations.contains(location))) {
+                locations.add(location);
+            }
+        }
+        locationComboBox.setItems(locations);
+
+    }
 
     }
 
--- Model/Reports.java
+++ Model/Reports.java
@@ -1,5 +1,6 @@
 package Model;
 
+import java.sql.Timestamp;
 import java.time.Month;
 
 public class Reports {
@@ -7,10 +8,31 @@ public class Reports {
     private String type;
     private int count;
 
+    private int appointmentID;
+    private String title;
+    private String description;
+    private String location;
+    private Timestamp start;
+    private Timestamp end;
+    private int customerID;
+    private int contactID;
+
     public Reports(Month month, String type, int count){
       setMonth(month);
       setType(type);
       setCount(count);
+    }
+    public Reports(int appointmentID,String title,String description,String location,String type,Timestamp start,Timestamp end,int customerID,int contactID){
+
+        setType(type);
+        setAppointmentID(appointmentID);
+        setContactID(contactID);
+        setTitle(title);
+        setDescription(description);
+        setLocation(location);
+        setStart(start);
+        setEnd(end);
+        setCustomerID(customerID);
     }
 
     public String getType() {
@@ -24,6 +46,72 @@ public class Reports {
     public Month getMonth() {
         return month;
     }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getAppointmentID() {
+        return appointmentID;
+    }
+
+    public int getContactID() {
+        return contactID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public Timestamp getEnd() {
+        return end;
+    }
+
+    public Timestamp getStart() {
+        return start;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAppointmentID(int appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public void setContactID(int contactID) {
+        this.contactID = contactID;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setStart(Timestamp start) {
+        this.start = start;
+    }
+
+    public void setEnd(Timestamp end) {
+        this.end = end;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
 
     public void setCount(int count) {
         this.count = count;
